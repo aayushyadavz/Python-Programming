3b0a49e271a9917835192e48fdb5130854c22a6e
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-04-24 18:36:31 +0530

    first commit

---
+++ Day 1/task.py
@@ -0,0 +1,27 @@
+# 1. Print function
+# print("Hello World!\nHello World!")
+
+# 2. String Concatenation
+# print("Hello" + " " + "Ayush!")
+
+# 3. Input Function
+# input("What is your name?")
+# print("Hello " + input("What is your name?") + "!")
+
+# 4. Variables
+# name = input("What is your name?")
+# print(name)
+
+# Checking the length of user input
+# print(len(input("What is your name?")))
+
+# Split everything into variables
+# username = input("What is your name?")
+# length = len(username)
+# print(length)
+
+# Band Name Generator Project
+print("Welcome to the Band Name Generator.")
+user_city_name = input("What's the name of the city you grew up in? \n")
+user_pet_name  = input("What's your pet's name? \n")
+print("Your band name could be " + user_city_name + " " + user_pet_name)
