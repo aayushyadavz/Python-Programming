f2929b0cc1562e236a79bd121fb183bd536e4999
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-04-26 17:11:12 +0530

    Day 2

---
+++ Day 2/task.py
@@ -0,0 +1,34 @@
+# 1.Subscripting
+# print("Ayush"[0]) 
+# print("Ayush"[-1]) # Get hold of the last character in the string 
+
+# 2. Integar - Whole number
+# print(123 + 345)
+# Large Integars - eg. 123,456,789
+# print(123_456_789) # commas replaced with "_" in python 
+
+# 3. Float - floating point number
+# print(3.14159) 
+
+# Boolean
+# print(True)
+# print(False)
+
+# Note: The len() function takes arguments such as strings.
+
+# 4. Type checking
+# print(type("xyz"))
+# print(type(123))
+# print(type(True))
+# print(type(3.2))
+
+# 5. Type conversion
+# print(int("123") + int("456"))
+# print(int("abc") + int("456")) # ValueError
+# Other -
+# str()
+# float()
+# bool()
+
+# Task - print("Number of letters in your name: " + len(input("Enter your name")))
+# print("Number of letters in your name: " + str(len(input("Enter your name"))))
