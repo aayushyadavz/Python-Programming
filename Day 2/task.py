68afb1e39398871f0485bf077a477b7e2247f194
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-04-28 19:06:59 +0530

    Project: Tip Calculator

--- Day 2/task.py
+++ Day 2/task.py
@@ -80,4 +80,14 @@ score = 0
 height = 1.8
 is_winning = True
 # print("Your score is " + str(score) + ", your height is " + str(height) +  ". You are winning is " + str(is_winning)) # instead of doing this
-print(f"Your score is {score}, your height is {height}. You are winning is {is_winning}")
+# print(f"Your score is {score}, your height is {height}. You are winning is {is_winning}")
+
+# Tip Calculator Project
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? ₹"))
+tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+bill_with_tip = tip / 100 * bill + bill
+bill_per_person = bill / people
+final_bill = round(bill_per_person, 2)
+print(f"Each person should pay: ₹{final_bill}")
